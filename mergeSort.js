27d70f5c71a14509e7e97010f5f3dc6609bac2ae
Shruti3120 <69119068+Shruti3120@users.noreply.github.com>
2022-04-21 23:17:56 +0530

    Add files via upload

---
+++ mergeSort.js
@@ -0,0 +1,59 @@
+
+(function() {
+
+  // expose to Array and jQuery
+  Array.prototype.mergeSort = jQuery.fn.mergeSort = mergeSort;
+
+  function mergeSort(compare) {
+
+    var length = this.length,
+        middle = Math.floor(length / 2);
+
+    if (!compare) {
+      compare = function(left, right) {
+        if (left < right)
+          return -1;
+        if (left == right)
+          return 0;
+        else
+          return 1;
+      };
+    }
+
+    if (length < 2)
+      return this;
+
+    return merge(
+      this.slice(0, middle).mergeSort(compare),
+      this.slice(middle, length).mergeSort(compare),
+      compare
+    );
+  }
+
+  function merge(left, right, compare) {
+
+    var result = [];
+
+    while (left.length > 0 || right.length > 0) {
+      if (left.length > 0 && right.length > 0) {
+        if (compare(left[0], right[0]) <= 0) {
+          result.push(left[0]);
+          left = left.slice(1);
+        }
+        else {
+          result.push(right[0]);
+          right = right.slice(1);
+        }
+      }
+      else if (left.length > 0) {
+        result.push(left[0]);
+        left = left.slice(1);
+      }
+      else if (right.length > 0) {
+        result.push(right[0]);
+        right = right.slice(1);
+      }
+    }
+    return result;
+  }
+})();
